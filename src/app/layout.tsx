90055712f3596f176aa2f668b6d7ac6eeaa4e49c
ryan0122 <your@email.com>
2026-01-14 07:15:58 -0700

    adding ga4 script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata = {
@@ -10,6 +11,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-65D61VFK6Q"/>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-65D61VFK6Q');
+         `}
+        </Script>
         {children}
       </body>
     </html>
